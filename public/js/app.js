974ebac2175ca447a2aefb23056e1a81a9fed7f0
Prafull Sakpal <prafullsakpal158@gmail.com>
2021-01-26 23:33:41 +0530

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,8 @@ weatherForm.addEventListener('submit', e => {
   messageTwo.textContent = '';
   
   const location = search.value;
-  fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+
+  fetch(`/weather?address=${location}`).then(response => {
     response.json().then(data => {
       messageOne.textContent = '';
       if (data.error) {
